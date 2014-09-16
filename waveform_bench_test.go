e47ffa10724b44fa29fa322b187a58222898c1c8
Matt Layher <mdlayher@gmail.com>
2014-09-16 19:08:26 -0400

    Add proper benchmarks for ComputeValues and ImageFromValues

--- waveform_bench_test.go
+++ waveform_bench_test.go
@@ -19,6 +19,46 @@ func BenchmarkNewFLAC(b *testing.B) {
 	benchmarkNew(b, flacFile, nil)
 }
 
+// BenchmarkComputeValuesWAV checks the performance of the ComputeValues() function with a WAV file
+func BenchmarkComputeValuesWAV(b *testing.B) {
+	benchmarkComputeValues(b, wavFile, nil)
+}
+
+// BenchmarkComputeValuesFLAC checks the performance of the ComputeValues() function with a FLAC file
+func BenchmarkComputeValuesFLAC(b *testing.B) {
+	benchmarkComputeValues(b, flacFile, nil)
+}
+
+// BenchmarkImageFromValues60 checks the performance of the ImageFromValues() function
+// with approximately 60 seconds of computed values
+func BenchmarkImageFromValues60(b *testing.B) {
+	benchmarkImageFromValues(b, 60, nil)
+}
+
+// BenchmarkImageFromValues120 checks the performance of the ImageFromValues() function
+// with approximately 120 seconds of computed values
+func BenchmarkImageFromValues120(b *testing.B) {
+	benchmarkImageFromValues(b, 120, nil)
+}
+
+// BenchmarkImageFromValues240 checks the performance of the ImageFromValues() function
+// with approximately 240 seconds of computed values
+func BenchmarkImageFromValues240(b *testing.B) {
+	benchmarkImageFromValues(b, 240, nil)
+}
+
+// BenchmarkImageFromValues480 checks the performance of the ImageFromValues() function
+// with approximately 480 seconds of computed values
+func BenchmarkImageFromValues480(b *testing.B) {
+	benchmarkImageFromValues(b, 480, nil)
+}
+
+// BenchmarkImageFromValues960 checks the performance of the ImageFromValues() function
+// with approximately 960 seconds of computed values
+func BenchmarkImageFromValues960(b *testing.B) {
+	benchmarkImageFromValues(b, 960, nil)
+}
+
 // BenchmarkRMSF64Samples22050 checks the performance of the RMSF64Samples() function
 // with 22050 samples
 func BenchmarkRMSF64Samples22050(b *testing.B) {
@@ -47,6 +87,21 @@ func BenchmarkRMSF64Samples176400(b *testing.B) {
 func benchmarkNew(b *testing.B, data []byte, options *Options) {
 	for i := 0; i < b.N; i++ {
 		New(bytes.NewReader(data), options)
+	}
+}
+
+// benchmarkComputeValues contains common logic for benchmarking ComputeValues
+func benchmarkComputeValues(b *testing.B, data []byte, options *ComputeOptions) {
+	for i := 0; i < b.N; i++ {
+		ComputeValues(bytes.NewReader(data), options)
+	}
+}
+
+// benchmarkImageFromValues contains common logic for benchmarking ImageFromValues
+func benchmarkImageFromValues(b *testing.B, count int, options *ImageOptions) {
+	values := make([]float64, count)
+	for i := 0; i < b.N; i++ {
+		ImageFromValues(values, options)
 	}
 }
 
