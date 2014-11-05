1bb29ed07a6ac1bffaf05d60cdf9ffeb237e9941
Matt Layher <mdlayher@gmail.com>
2014-11-04 19:34:10 -0500

    colorfunc_test: fix tests with new ColorFunc signature

--- colorfunc_test.go
+++ colorfunc_test.go
@@ -24,7 +24,7 @@ func TestSolidColor(t *testing.T) {
 
 	for i, c := range colors {
 		fn := SolidColor(c)
-		if out := fn(i, i, i); out != c {
+		if out := fn(i, i, i, i, i, i); out != c {
 			t.Fatalf("unexpected SolidColor color: %v != %v", out, c)
 		}
 	}
@@ -73,7 +73,7 @@ func testFuzzColor(t *testing.T, in []color.Color) {
 	// the input slice.
 	fn := FuzzColor(in...)
 	for i := 0; i < 10000; i++ {
-		if out, ok := set[fn(i, i, i).(color.RGBA)]; !ok {
+		if out, ok := set[fn(i, i, i, i, i, i).(color.RGBA)]; !ok {
 			t.Fatalf("color not in set: %v", out)
 		}
 	}
@@ -84,7 +84,7 @@ func testStripeColor(t *testing.T, in []color.Color, out []color.Color) {
 	// Validate that StripeColor produces expected output at each index
 	fn := StripeColor(in...)
 	for i := 0; i < len(out); i++ {
-		if c := fn(i, 0, 0); c != out[i] {
+		if c := fn(i, 0, 0, 0, 0, 0); c != out[i] {
 			t.Fatalf("[%02d] unexpected output color: %v != %v", i, c, out[i])
 		}
 	}
